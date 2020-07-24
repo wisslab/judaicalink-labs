3844745cc17360e3b41caf8cdfcebffd9752dbc3
sarafischer <sf.fischer.fs@gmail.com>
2020-07-24 11:45:00 +0200

    pie chart for dataslugs

--- labs/dashboard/views.py
+++ labs/dashboard/views.py
@@ -5,43 +5,28 @@ import json
 from backend.models import Dataset
 
 def test(request):
-    es = Elasticsearch()
-    body = {
-        "from" : 0, "size" : 10000,
-        "query" : {
-            "match_all" : {}
-        },
-        }
-    result = es.search(index="judaicalink", body = body)
-
-    total_hits = result ["hits"] ["total"] ["value"]
-
-    dataset_objects = Dataset.objects.all()
-    dataslug_to_dataset = {}
-    for i in dataset_objects:
-        dataslug_to_dataset [i.dataslug] = i.title
-
-    dataslugs_amaount = {}
-
-    for data in result ["hits"] ["hits"]:
-        dataslug = data ["_source"] ["dataslug"]
-        if dataslug in dataslugs_amaount:
-            dataslugs_amaount [dataslug] += 1
-        else:
-            if dataslug in dataslug_to_dataset:
-                dataslugs_amaount [dataslug] = 1
-
     list_dataslugs = []
     list_amount_data = []
 
-    for d in dataslugs_amaount:
-        list_dataslugs.append (d)
-        list_amount_data.append (dataslugs_amaount [d])
+    dataset_objects = Dataset.objects.all()
+    dataslugs = []
+    for i in dataset_objects:
+        if i.dataslug:
+            dataslugs.append (i.dataslug)
 
-    print (dataslugs_amaount)
-    print (list_dataslugs)
-    print (list_amount_data)
-    print (total_hits)
+    for dataslug in dataslugs:
+        es = Elasticsearch()
+        body = {
+            "query" : {
+                "match" : {
+                    "dataslug": dataslug
+                }
+            },
+        }
+        result = es.count(index="judaicalink", body = body)
+        amount = result ["count"]
+        list_dataslugs.append (dataslug)
+        list_amount_data.append (amount)
 
     context = {
         "result": json.dumps(result),
