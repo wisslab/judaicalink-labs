ba82df679b5a1ff04307af054f838f7c4e0e4f3c
sarafischer <sf.fischer.fs@gmail.com>
2020-08-30 22:20:21 +0200

    interact with barchart by input (setting timespan for shown data) dashboard_main.html und views.py aktualisiert

--- labs/dashboard/views.py
+++ labs/dashboard/views.py
@@ -5,33 +5,61 @@ import json
 from backend.models import Dataset
 
 def test(request):
-    list_dataslugs = []
-    list_amount_data = []
 
-    dataset_objects = Dataset.objects.all()
-    dataslugs = []
-    for i in dataset_objects:
-        if i.dataslug:
-            dataslugs.append (i.dataslug)
-
-    for dataslug in dataslugs:
-        es = Elasticsearch()
-        body = {
-            "query" : {
-                "match" : {
-                    "dataslug": dataslug
+    es = Elasticsearch()
+    body = {
+        'size': '0',
+        'aggs': {
+            'uniq_year': {
+                'terms': {
+                    'field': 'birthYear', 'size': 1000000
                 }
-            },
+            }
         }
-        result = es.count(index="judaicalink", body = body)
-        amount = result ["count"]
-        list_dataslugs.append (dataslug)
-        list_amount_data.append (amount)
+    }
+    result = es.search(index="judaicalink", body = body)
+
+    aggregation = result ["aggregations"] ["uniq_year"] ["buckets"]
+    aggregation.sort(key=lambda r: r['key'])
+    aggregation = json.dumps (aggregation)
+
+    birthyears = []
+    amount_of_people = []
+
+    for i in result ["aggregations"] ["uniq_year"] ["buckets"]:
+        birthyears.append (i ["key"])
+        amount_of_people.append (i ["doc_count"])
+
+    # list_dataslugs = []
+    # list_amount_data = []
+    #
+    # dataset_objects = Dataset.objects.all()
+    # dataslugs = []
+    # for i in dataset_objects:
+    #     if i.dataslug:
+    #         dataslugs.append (i.dataslug)
+    #
+    # for dataslug in dataslugs:
+    #     es = Elasticsearch()
+    #     body = {
+    #         "query" : {
+    #             "match" : {
+    #                 "dataslug": dataslug
+    #             }
+    #         },
+    #     }
+    #     result = es.count(index="judaicalink", body = body)
+    #     amount = result ["count"]
+    #     list_dataslugs.append (dataslug)
+    #     list_amount_data.append (amount)
 
     context = {
-        "result": json.dumps(result),
-        "list_dataslugs": list_dataslugs,
-        "list_amount_data": list_amount_data,
+        #"result": json.dumps(result),
+        #"list_dataslugs": list_dataslugs,
+        #"list_amount_data": list_amount_data,
+        #'birthyears': birthyears,
+        #'amount_of_people': amount_of_people,
+        'aggregation': aggregation,
     }
 
     return render(request, "dashboard/dashboard_main.html", context)
